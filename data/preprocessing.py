cec98d81db2562c18b7e27617590fb8f57cef4d3
Pprajapati7964 <pprajapati256i@gmail.com>
2026-01-02 14:14:40 -0600

    created preprocessing scripts and VE

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -1,7 +1,17 @@
-import os
 import pandas as pd 
-import numpy as np 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from shapely.geometry import Point 
+import warnings
+warnings.filterwarnings('ignore')
+plt.style.use("dark_background")
 
 
 df = pd.read_csv('raw/earthquake_data_tsunami.csv')
-df.drop(columns=['nst', 'dmin', 'gap', 'month'], inplace = True)
+df.head()
